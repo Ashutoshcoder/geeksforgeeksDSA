4801871f318c34798fc30bc88235bb0aa39701b5
Ashutoshcoder <ashutoshkumardbms@gmail.com>
2020-07-02 09:51:22 +0530

    Rotate Array by D times towards Left

---
+++ Arrays/RotateArrayByDLeft.java
@@ -0,0 +1,78 @@
+package geeksforgeeksDSA.geeksforgeeksDSA.Arrays;/*
+Author : Ashutosh Kumar
+Date : 27/06/20 
+Email : ashutoshkumardbms@gmail.com
+Published on : 2020,June,Saturday
+Description :
+*/
+
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFGG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for(int i=0;i<t;i++)
+        {
+            String str=br.readLine();
+            String[] starr=str.split(" ");
+            int n=Integer.parseInt(starr[0]);
+            int d= Integer.parseInt(starr[1]);
+
+            int[] arr=new int[n];
+            String str1=br.readLine();
+            String[] starr1=str1.split(" ");
+            for(int j=0;j<n;j++)
+            {
+                arr[j]= Integer.parseInt(starr1[j]);
+            }
+
+            new Solution().rotateArr(arr, d, n);
+            StringBuffer sb=new StringBuffer();
+            for(int t1=0;t1<n;t1++)
+                sb.append(arr[t1]+" ");
+            System.out.println(sb);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static void rotateArr(int arr[], int d, int n)
+    {
+        // add your code here
+        //reverse 1st d elements
+        reverse(arr,0,d-1);
+
+        //reverse remaining elements
+        reverse(arr,d,n-1);
+
+        //reverse all elements
+        reverse(arr,0,n-1);
+    }
+
+    static int[] reverse(int arr[],int low, int high)
+    {
+        int temp=0;
+        while(low<high)
+        {
+            temp=arr[low];
+            arr[low]=arr[high];
+            arr[high]=temp;
+            low++;
+            high--;
+        }
+        return arr;
+    }
+}
