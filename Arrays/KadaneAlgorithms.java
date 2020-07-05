8bd19f7b7d9d850e11417dfa715cc701b557bc58
Ashutoshcoder <ashutoshkumardbms@gmail.com>
2020-07-05 18:29:51 +0530

    Kadane Algorithm

---
+++ Arrays/KadaneAlgorithms.java
@@ -0,0 +1,94 @@
+package geeksforgeeksDSA.geeksforgeeksDSA.Arrays;
+/*
+Author : Ashutosh Kumar
+Date : 01/07/20 
+Email : ashutoshkumardbms@gmail.com
+Published on : 2020,July,Wednesday
+Description :
+Given an array arr of N integers. Find the contiguous sub-array with maximum sum.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases.
+The description of T test cases follows.
+The first line of each test case contains a single integer N denoting the size of the array.
+The second line contains N space-separated integers A1, A2, ..., AN denoting the elements of the array.
+
+Output:
+Print the maximum sum of the contiguous sub-array in a separate line for each test case.
+
+User Task:
+The task is to complete the function maxSubarraySum() which finds subarray with maximum sum.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 ≤ T ≤ 110
+1 ≤ N ≤ 106
+-107 ≤ A[i] <= 107
+
+Example:
+Input:
+2
+5
+1 2 3 -2 5
+4
+-1 -2 -3 -4
+
+Output:
+9
+-1
+
+Explanation:
+Testcase 1: Max subarray sum is 9 of elements (1, 2, 3, -2, 5) which is a contiguous subarray.
+
+
+*/
+
+// { Driver Code Starts
+import java.io.*;
+
+class Arrayzzz {
+
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            Kadanezz obj = new Kadanezz();
+
+            System.out.println(obj.maxSubarraySum(arr, n));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Kadanezz{
+
+    // Function to find subarray with maximum sum
+    // arr: input array
+    // n: size of array
+    int maxSubarraySum(int arr[], int n)
+    {
+
+        // Your code here
+        int maxSum=arr[0],res=arr[0];
+        for(int i=1;i<n;i++)
+        {
+            maxSum=Math.max(maxSum+arr[i],arr[i]);
+            res=Math.max(res,maxSum);
+        }
+        return res;
+    }
+
+}
+
+
